84bcfac49e8cce481af8e93db698c3ee9d1a1128
Riccardo Pittau <elfosardo@gmail.com>
2025-05-15 18:27:40 +0200

    Use Python 3.12

--- prepare-image.sh
+++ prepare-image.sh
@@ -34,12 +34,12 @@ if  [[ -f /tmp/main-packages-list.ocp ]]; then
     fi
 
     ### source install ###
-    BUILD_DEPS="python3-devel gcc gcc-c++ python3-wheel"
+    BUILD_DEPS="python3.12-devel gcc gcc-c++ python3.12-wheel"
 
     # NOTE(elfosardo): wheel is needed because of pip "no-build-isolation" option
     # setting installation of setuptoools here as we may want to remove it
     # in teh future once the container build is done
-    dnf install -y python3-pip 'python3-setuptools >= 64.0.0' $BUILD_DEPS
+    dnf install -y python3.12-pip 'python3.12-setuptools >= 64.0.0' $BUILD_DEPS
 
     # NOTE(elfosardo): --no-index is used to install the packages emulating
     # an isolated environment in CI. Do not use the option for downstream
@@ -67,12 +67,12 @@ if  [[ -f /tmp/main-packages-list.ocp ]]; then
     # See https://issues.redhat.com/browse/METAL-1049 for more details.
     PIP_SOURCES_DIR="all_sources"
     mkdir $PIP_SOURCES_DIR
-    python3 -m pip download --no-build-isolation --no-deps -r "${REQS}" -d $PIP_SOURCES_DIR
-    python3 -m pip install $PIP_OPTIONS --prefix /usr -r "${REQS}" -f $PIP_SOURCES_DIR
+    python3.12 -m pip download --no-build-isolation --no-deps -r "${REQS}" -d $PIP_SOURCES_DIR
+    python3.12 -m pip install $PIP_OPTIONS --prefix /usr -r "${REQS}" -f $PIP_SOURCES_DIR
 
     # NOTE(janders) since we set --no-compile at install time, we need to
     # compile post-install (see RHEL-29028)
-    python3 -m compileall --invalidation-mode=timestamp -q /usr
+    python3.12 -m compileall --invalidation-mode=timestamp -q /usr
 
     # ironic system configuration
     mkdir -p /var/log/ironic /var/lib/ironic
